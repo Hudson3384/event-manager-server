7052eb10e8fc8ef04bf4bfb4b78a21857a4906d2
hudson <hudson.arruda@outlook.com>
2025-01-02 14:33:15 -0300

    🔥 Remove unused imports on routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers;
 use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('auth', [AuthController::class, 'redirectToAuth']);
